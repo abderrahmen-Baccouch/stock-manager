3fce10e8b3267e6366ba455bb0219b8a3f8503ac
abderrahmen <abdoubaccouch42@gmail.com>
2022-01-07 17:00:37 +0100

    projet mobile

---
+++ app/src/main/java/com/example/myapplication/Article.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+public class Article {
+    public String name,price;
+
+    public Article(){
+
+    }
+    public Article(String name,String price){
+
+        this.name=name;
+        this.price=price;
+
+
+
+    }
+
+}
